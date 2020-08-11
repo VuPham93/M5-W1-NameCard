523fbb02d27b1336ae1792a2c28f515ca73657be
VuPham93 <62596412+VuPham93@users.noreply.github.com>
2020-08-11 14:36:45 +0700

    Add card

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  @Input() cardName: string;
+  @Input() email: string;
+  @Input() phone: string;
+  @Input() image: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
